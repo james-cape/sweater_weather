c1630398324a83d8f1839e32308955b85c873a02
James Cape <james.cape@gmail.com>
2019-07-28 10:25:06 -0600

    Changes forecast index to forecast show in routes and controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ForecastController < Api::V1::ApiBaseController
 
-  def index
+  def show
     coordinates = CoordinateService.new.get_results(params[:location])
 require 'pry'; binding.pry
     forecast = ForecastService.new(coordinates)
 
-
+    filtered_forecast = ForecastSerializer.new(forecast)
     # render status: 201, json: {}
   end
 end
