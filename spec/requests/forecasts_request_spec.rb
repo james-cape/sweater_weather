7b5e4db54bbbe98d74fe6fa51d38e553c562b769
James Cape <james.cape@gmail.com>
2019-07-28 17:00:18 -0600

    Adds request spec for forecast

--- spec/requests/forecasts_request_spec.rb
+++ spec/requests/forecasts_request_spec.rb
@@ -2,18 +2,52 @@ require 'rails_helper'
 
 describe 'coordinates API' do
   it 'gets a forecast for the city and state' do
-    # coordinate_1 = create(:coordinate)
-    # create(:coordinate)
-    # create(:coordinate)
 
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
 
-    forecast = JSON.parse(response.body)
-    # expect(coordinates["data"].count).to eq(3)
-    # expect(coordinates["data"][0]["attributes"]["creator"]).to eq(coordinate_1.creator)
-    # expect(coordinates["data"][0]["attributes"]["commit_coordinates"]).to eq(coordinate_1.commit_coordinates)
+    forecast = JSON.parse(response.body, symbolize_names: true)
 
+    expect(forecast.keys).to contain_exactly(
+      :snapshot,
+      :details,
+      :hourly_data,
+      :daily_data
+    )
+
+    expect(forecast[:snapshot].keys).to contain_exactly(
+      :summary,
+      :temperature,
+      :temperature_high,
+      :temperature_low,
+      :city_state,
+      :country,
+      :time_date
+    )
+
+    expect(forecast[:details].keys).to contain_exactly(
+      :short_summary,
+      :long_summary,
+      :apparent_temperature,
+      :humidity,
+      :visibility,
+      :uv_index,
+      :uv_index_risk
+    )
+
+    expect(forecast[:hourly_data].keys).to contain_exactly(
+      :temperature
+    )
+
+    expect(forecast[:daily_data].keys).to contain_exactly(
+      :icon,
+      :precip_probability,
+      :temperature_high,
+      :temperature_low
+    )
+
+    expect(forecast[:hourly_data][:temperature].length).to eq(8)
+    expect(forecast[:daily_data][:icon].length).to eq(5)
   end
 end
