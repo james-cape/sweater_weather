317f64c24d3840f11adb7318eb10a9d942bf99be
James Cape <james.cape@gmail.com>
2019-07-28 10:24:51 -0600

    Tests forecast service

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -4,9 +4,23 @@ describe ForecastService do
   it 'turns lat/long into forecast in json' do
     service = ForecastService.new
 
-    forecast = service.get_results({lat: 39.7392358, lng: -104.990251})
-# require 'pry'; binding.pry
-    expect(forecast).to have_key(:lat)
-    expect(forecast).to have_key(:lng)
+    coordinates = {lat: 39.7392358, lng: -104.990251}
+    forecast = service.get_results(coordinates)
+
+    expect(forecast[:currently]).to have_key(:summary)
+    expect(forecast[:currently]).to have_key(:temperature)
+
+    expect(forecast[:hourly][:data][0]).to have_key(:apparentTemperature)
+    expect(forecast[:hourly][:data][0]).to have_key(:temperature)
+
+    expect(forecast[:daily][:data][0]).to have_key(:temperatureHigh)
+    expect(forecast[:daily][:data][0]).to have_key(:temperatureLow)
+    expect(forecast[:daily][:data][0]).to have_key(:humidity)
+    expect(forecast[:daily][:data][0]).to have_key(:visibility)
+    expect(forecast[:daily][:data][0]).to have_key(:uvIndex)
+    expect(forecast[:daily][:data][0]).to have_key(:precipProbability)
+    expect(forecast[:daily][:data][0]).to have_key(:uvIndex)
+    expect(forecast[:daily][:data][0]).to have_key(:icon)
+    expect(forecast[:daily]).to have_key(:summary)
   end
 end
