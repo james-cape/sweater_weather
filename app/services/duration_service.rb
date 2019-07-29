ef016a6796d01e1f35cc2337019e8a3bfbb21aae
James Cape <james.cape@gmail.com>
2019-07-29 09:34:42 -0600

    Figures out hash pattern for duration

--- app/services/duration_service.rb
+++ app/services/duration_service.rb
@@ -2,12 +2,12 @@ class DurationService
 
   def initialize; end
 
-  def get_results(start, finish)
+  def get_duration(start, finish)
     data = {
       start: start,
       finish: finish
     }
-    results = get_json("directions/json", data)
+    get_json("directions/json", data)[:routes][0][:legs][0][:duration]
 require 'pry'; binding.pry
     # citystate = "#{results[:address_components][0][:short_name]}, #{results[:address_components][2][:short_name]}"
     # country = results[:address_components][3][:long_name]
@@ -28,6 +28,8 @@ require 'pry'; binding.pry
 
   def conn
     Faraday.new('https://maps.googleapis.com/maps/api/') do |f|
+      f.params['origin'] = 'denver,co'
+      f.params['destination'] = 'pueblo,co'
       f.params['key'] = ENV['GEOCODE_GOOGLE_API_KEY']
       f.adapter Faraday.default_adapter
     end
