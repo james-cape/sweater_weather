ab4a06829cb52a55b6faa5caa304be9a0ed300f0
James Cape <james.cape@gmail.com>
2019-07-29 10:40:04 -0600

    Functional results, want to put destination into conn method

--- app/services/restaurant_service.rb
+++ app/services/restaurant_service.rb
@@ -5,22 +5,23 @@ class RestaurantService
   def get_restaurants(restaurant_params, duration)
 
     # eta = (Time.now + 4.hours).to_i
-    results = get_json("search?location=#{restaurant_params[:end]}")
+    results = get_json("search?location=#{restaurant_params[:end]}", duration[:value])
     require 'pry'; binding.pry
     # results.map { |result| result[:url_o] }.compact!.first(quantity)
   end
 
   private
 
-  def get_json(path)
-    response = conn.get(path)
+  def get_json(path, time_in_sec)
+    response = conn(time_in_sec).get(path)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def conn
+  def conn(time_in_sec)
     Faraday.new('https://api.yelp.com/v3/businesses/') do |f|
-      f.params['open_at'] = (Time.now + 4.hours).to_i
-      f.params['term'] = 'restaurants'
+      f.params['open_at'] = (Time.now + time_in_sec / (60 * 60).hours).to_i
+      f.params['categories'] = 'restaurants'
+      f.params['limit'] = 3
       # f.params['location'] = 'denver,co'
       f.authorization :Bearer, "#{ENV['YELP_API_KEY']}"
       f.adapter Faraday.default_adapter
