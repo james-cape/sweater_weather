5a59c5c684aeae3766363d633717e618368dcb62
James Cape <james.cape@gmail.com>
2019-07-29 11:27:07 -0600

    Gives time calculation its own method

--- app/services/restaurant_service.rb
+++ app/services/restaurant_service.rb
@@ -18,19 +18,23 @@ class RestaurantService
 
   private
 
-  def get_json(path, time_in_sec, location)
-    response = conn(time_in_sec, location).get(path)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn(time_in_sec, location)
-    Faraday.new('https://api.yelp.com/v3/businesses/') do |f|
-      f.params['open_at'] = (Time.now + time_in_sec / (60 * 60).hours).to_i
-      f.params['categories'] = 'chinese'
-      f.params['limit'] = 3
-      f.params['location'] = location
-      f.authorization :Bearer, "#{ENV['YELP_API_KEY']}"
-      f.adapter Faraday.default_adapter
+    def get_json(path, time_in_sec, location)
+      response = conn(time_in_sec, location).get(path)
+      JSON.parse(response.body, symbolize_names: true)
     end
-  end
+
+    def conn(time_in_sec, location)
+      Faraday.new('https://api.yelp.com/v3/businesses/') do |f|
+        f.params['open_at'] = arrival_time(time_in_sec)
+        f.params['categories'] = 'chinese'
+        f.params['limit'] = 3
+        f.params['location'] = location
+        f.authorization :Bearer, "#{ENV['YELP_API_KEY']}"
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def arrival_time(time_in_sec)
+      (Time.now + time_in_sec / (60 * 60).hours).to_i
+    end
 end
